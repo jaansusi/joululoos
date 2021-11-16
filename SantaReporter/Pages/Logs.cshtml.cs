04930e4c76f6ffb75153097ff0b2334a93b607c0
Jaan Susi <susi@axinom.com>
2021-11-16 15:44:36 +0200

    Veebiprojektile üleminek tasakesi

---
+++ SantaReporter/Pages/Logs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SantaReporter.Pages
+{
+    public class LogsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

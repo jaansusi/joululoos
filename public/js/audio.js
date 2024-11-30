c07804b5fe07a5904713cf9b37ab8c779ac18b06
Jaan Susi <jaan.susi@gmail.com>
2024-11-30 02:38:52 +0200

    #12 - Removed audio control button, Santa can do it

--- public/js/audio.js
+++ public/js/audio.js
@@ -5,16 +5,11 @@ function playPause() {
     audio.volume = 0.2;
 
     if (audioPlaying) {
-        document.getElementById("pauseIcon").classList.add('hidden');
-        document.getElementById("playIcon").classList.remove('hidden');
         audioPlaying = false;
         audio.pause();
     }
     else {
-        document.getElementById("pauseIcon").classList.remove('hidden');
-        document.getElementById("playIcon").classList.add('hidden');
-        audio.play();
-        document.getElementById('audioContainer').classList.remove('hidden');
         audioPlaying = true;
+        audio.play();
     }
 }
